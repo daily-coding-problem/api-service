b9387bb13971ab0cb5ea2233ddf704a87a3ed2a3
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-09-08 11:26:33 -0400

    Add cron_jobs table to schema.sql
    This commit creates a new table named `cron_jobs` with columns for job details such as name, description, schedule, and timestamps for tracking execution. The table includes a primary key and a boolean to indicate if the job is enabled.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS leetcode;
+CREATE TABLE cron_jobs
+(
+	id          SERIAL PRIMARY KEY,
+	name        VARCHAR(255),
+	description TEXT,
+	schedule    VARCHAR(255),
+	enabled     BOOLEAN DEFAULT TRUE,
+	last_run_at TIMESTAMP,
+	next_run_at TIMESTAMP
+);
